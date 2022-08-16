0c6683e694787a93e010ff0a216d2ec4034005b0
santiagoprueba <santiago@gmail.com>
2022-08-16 16:20:50 -0500

    useReducer

--- src/componentes/Characters.jsx
+++ src/componentes/Characters.jsx
@@ -1,7 +1,31 @@
-import React, { useState, useEffect } from "react";
+//Importamos los Hooks
+import React, { useState, useEffect, useReducer } from "react";
 
+//Estado inicial
+const initialState = {
+  favorites: [],
+};
+
+//Reducer
+const favoriteReducer = (state, action) => {
+  //Switch
+  switch (action.type) {
+    //Caso 1
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+    //Caso por default
+    default:
+      return state;
+  }
+};
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+
+  //useReducer para la variable Favorites, Parametros: (Reducer, Estado Inicial)
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
   useEffect(() => {
     //Petición fetch
@@ -11,11 +35,31 @@ const Characters = () => {
       //Entonces esos los resultados de esos datos envielos a la variable del estado characters
       .then((data) => setCharacters(data.results));
   }, []);
+
+  const handleClick = (favorite) => {
+    //Llama al dispactch, le entrega el tipo de acción a realizar y el payload con la información a actualizar
+    dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
+  };
+
   return (
     <div className="Characters">
-      {/* Haga un mapeo de la variable de estado characters y por cada character retorne un h2 con el nombre del character */}
+      {/* Haga un mapeo de la variable favorites de InitialState */}
+      {favorites.favorites.map(favorite => (
+        <li key={favorite.id}>
+          {favorite.name}
+        </li>
+      ))}
+
+        {/* Haga un mapeo de la variable characters de useState */}
       {characters.map((character) => (
-        <h2>{character.name}</h2>
+        //Renderizamos un item con un ID diferente
+        <div className="item" key={character.id}>
+          <h2>{character.name}</h2>
+          {/* Boton que invoca la función que agrega a favoritos (Le envía un Character) */}
+          <button type="button" onClick={() => handleClick(character)}>
+            Agregar a Favoritos
+          </button>
+        </div>
       ))}
     </div>
   );
